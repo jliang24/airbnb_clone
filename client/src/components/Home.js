e27ef08c8e61ff65a52a10866a95ffd8b07b95c5
jliang24 <jeffliang@nevada.unr.edu>
2019-05-03 19:07:51 -0700

    add accordion to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import requireAuth from 'components/requireAuth';
 import ListingCards from 'components/Listing/View/ListingCards';
+import 'css/accordion.css';
 
 class Home extends Component {
+  state = { listingsCollapsed: false, requestsCollapsed: false };
+
   render() {
     return (
-      <div style={{ marginTop: '20px' }}>
-        <h2 className="ui dividing header">
+      <div
+        style={{
+          marginTop: '20px'
+        }}
+      >
+        <h2
+          onClick={() => {
+            this.setState({ listingsCollapsed: !this.state.listingsCollapsed });
+          }}
+          className="ui dividing header accord"
+        >
           <div className="content">
             <i className="home icon" />
             My Listings
           </div>
+          <i className="dropdown icon" />
         </h2>
-        <div className="content">
+        <div className={this.state.listingsCollapsed ? 'collapsed' : 'section'}>
           <ListingCards user />
+        </div>
+        <h2
+          onClick={() => {
+            this.setState({ requestsCollapsed: !this.state.requestsCollapsed });
+          }}
+          className="ui dividing header accord"
+        >
+          <div className="content">
+            <i className="envelope open icon" />
+            My Requests
+          </div>
+          <i className="dropdown icon" />
+        </h2>
+        <div className={this.state.requestsCollapsed ? 'collapsed' : 'section'}>
+          <div>In progress...</div>
         </div>
       </div>
     );
