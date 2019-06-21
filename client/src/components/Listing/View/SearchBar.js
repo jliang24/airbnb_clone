4c5ae8a2192ff33aa708b819fded0fc0196b695b
jliang24 <jeffliang@nevada.unr.edu>
2019-06-20 19:43:59 -0700

    added cities and states JSON object

--- client/src/components/Listing/View/SearchBar.js
+++ client/src/components/Listing/View/SearchBar.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm, change } from 'redux-form';
+import * as cities from 'utils/cities';
+import * as states from 'utils/states';
 
 import 'css/searchbar.css';
 
+const initialState = {
+  results: []
+};
+
 class SearchBar extends Component {
-  render() {
+  state = initialState;
+
+  handleSearchChange = (e, newValue) => {
+    this.setState({ value: newValue });
+    console.log(cities);
+    console.log(states);
+  };
+
+  handleResultSelected = () => {
+    console.log('hey');
+    this.props.dispatch(change('searchbar', 'search', 'this is jeff'));
+  };
+
+  renderInput = field => {
     return (
       <div className="ui category search">
         <div className="ui icon input">
           <input
+            {...field.input}
             className="prompt"
             type="text"
             placeholder="Search by State, City, or Address..."
@@ -17,7 +38,19 @@ class SearchBar extends Component {
         <div className="results" />
       </div>
     );
+  };
+
+  render() {
+    return (
+      <Field
+        onChange={(e, newValue) => this.handleSearchChange(e, newValue)}
+        name="search"
+        component={this.renderInput}
+      />
+    );
   }
 }
 
-export default SearchBar;
+export default reduxForm({
+  form: 'searchbar'
+})(SearchBar);
