634d024165f3ce28a4bd06d9a841d3054a909823
jliang24 <jeffliang@nevada.unr.edu>
2020-11-12 01:54:22 -0800

    added keys to prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   secret: process.env.secret,
   awsAccessKeyId: process.env.awsAccessKeyId,
   awsSecretAccesskey: process.env.awsSecretAccesskey,
-  mongoURI: process.env.mongoURI
+  mongoURI: process.env.mongoURI,
+  cloudAPIKey: process.env.cloudAPIKey,
+  cloudSecret: process.env.cloudSecret
 };
