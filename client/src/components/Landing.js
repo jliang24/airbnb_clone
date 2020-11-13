f638e89facf8913a5a786c0038e552c3b1a83df9
jliang24 <jeffliang@nevada.unr.edu>
2020-11-12 19:03:13 -0800

    updated landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React from "react";
+import "css/landing.css";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
-    <div className="ui vertical inverted masthead center aligned segment">
-      <div className="inverted ui text container segment">
-        <h1
-          style={{ color: 'white', fontSize: '3rem' }}
-          className="inverted ui center aligned "
-        >
-          Welcome to an airbnb clone.
-        </h1>
-        <h2 className="grey inverted ui center aligned ">
-          Feel free to check out the listings at the top!
-        </h2>
+    <div className="pusher">
+      <div
+        id="landing"
+        className="ui vertical inverted masthead center aligned segment"
+      >
+        <div className="ui text container">
+          <h1 className="ui inverted header">airbnb clone</h1>
+          <h2>
+            Find adventures nearby or in faraway places and access unique homes,
+            experiences, and places around the world.
+          </h2>
+          <Link className="ui huge positive button" to="/signup">
+            <i className="pencil icon"></i>Sign up
+          </Link>
+          <Link className="ui huge primary button" to="/listings">
+            View Listings <i className="right arrow icon"></i>
+          </Link>
+        </div>
       </div>
     </div>
   );
